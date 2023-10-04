31e2db6b89b92dae42ca7de4b5543829bcf20bec
Duong Tien Dat <duongtiendat3103@gmail.com>
2023-10-04 23:32:45 +0700

    feat: khung app tính diem bai

--- src/views/modal-create/index.vue
+++ src/views/modal-create/index.vue
@@ -1,27 +1,16 @@
 <template>
-  <n-modal v-model:show="showModal">
-    <n-card
-      :style="styleModal"
-      :bordered="false"
-      size="huge"
-      role="dialog"
-      aria-modal="true">
-      <template #header-extra> Oops! </template>
-      Content
-      <template #footer> Footer </template>
-    </n-card>
-  </n-modal>
+
+  <n-drawer v-model:show="showModal" :width="502">
+    <n-drawer-content title="Stoner" closable>
+      Stoner is a 1965 novel by the American writer John Williams.
+    </n-drawer-content>
+  </n-drawer>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
 
 const showModal = ref<boolean>(false);
 
-const styleModal = {
-  width: "300px",
-  maxHeight: "300px",
-  display: "inline-block",
-};
 
 defineExpose({ showModal });
 </script>
